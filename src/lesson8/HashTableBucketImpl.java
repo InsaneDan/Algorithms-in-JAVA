2727421e8dbe0d63658f51ae25beaa34e165009e
IDA <isakov.danila.79@gmail.com>
2021-03-04 16:35:42 +0200

    Java Algorithms. Lesson 8 homework (HashTable bucket)

--- src/lesson8/HashTableBucketImpl.java
+++ src/lesson8/HashTableBucketImpl.java
@@ -1,7 +1,5 @@
 package lesson8;
 
-import com.sun.source.tree.LiteralTree;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -42,8 +40,9 @@ public class HashTableBucketImpl<K, V> implements HashTable<K, V> {
 
     /**
     * учитываем ограничения дженериков при создании массивов - type erasure => нельзя создать массив неизвестного типа;
-    * поэтому - ArrayList и заполнить его null, чтобы получить нужную длину
+    * поэтому - ArrayList и при инициализации заполнить его null, чтобы получить нужную длину
     * др.вариант: array = (T[])Array.newInstance(TClass, 10);*/
+
     private final ArrayList<LinkedList<Item<K, V>>> data;
     private int size;
 
@@ -63,69 +62,51 @@ public class HashTableBucketImpl<K, V> implements HashTable<K, V> {
     public boolean put(K key, V value) {
         int index = hashFunc(key);
         LinkedList<Item<K, V>> bucket;
-
-        // если в ячейке массива есть значение - получаем список
-        if (data.get(index) != null) {
+        if (data.get(index) != null) {                                      // если в ячейке массива есть значение - получаем список
             bucket = data.get(index);
         } else {
-            // если ячейка null, создаем в ней новый экземпляр LinkedList
-            bucket = new LinkedList<Item<K, V>>();
+            bucket = new LinkedList<Item<K, V>>();                          // если ячейка null, создаем в ней новый экземпляр LinkedList
         }
-
-        // проверяем ключи всех элементов в bucket
-        for (Item<K, V> item : bucket) {
-            if (item.getKey().equals(key)) { // если ключ найден - обновляем значение
+        for (Item<K, V> item : bucket) {                                    // проверяем ключи всех элементов в bucket
+            if (item.getKey().equals(key)) {                                // если ключ найден - обновляем значение
                 item.setValue(value);
                 return true;
             }
         }
-        // перебрали все элементы, но ключ не найден - добавляем к списку
-        bucket.add(new Item<K, V>(key, value));
-
-        //кладем список обратно в массив
-        data.set(index, bucket);
-        //добавляем размер
-        size++;
-        // возвращаем результат
-        return true;
+        bucket.add(new Item<K, V>(key, value));                             // перебрали все элементы, но ключ не найден - добавляем к списку
+        data.set(index, bucket);                                            //кладем список обратно в массив
+        size++;                                                             //добавляем размер
+        return true;                                                        // возвращаем результат
     }
 
     @Override
     public V get(K key) {
-        // определяем индекс
-        int index = hashFunc(key);
-        // нет данных - возвращаем null
-        if (data.get(index) == null) return null;
-        // проверяем ключи всех элементов в bucket
-        LinkedList<Item<K, V>> bucket = data.get(index);
+        int index = hashFunc(key);                                          // определяем индекс
+        if (data.get(index) == null) return null;                           // нет данных - возвращаем null
+        LinkedList<Item<K, V>> bucket = data.get(index);                    // проверяем ключи всех элементов в bucket
         for (Item<K, V> item : bucket) {
-            if (item.getKey().equals(key)) { // ключ найден
+            if (item.getKey().equals(key)) {                                // ключ найден
                 return item.getValue();
             }
         }
-        // ключ не найден
-        return null;
+        return null;                                                        // ключ не найден
     }
 
     @Override
     public V remove(K key) {
-        // возвращаемое значение (если будет найдено)
-        V val = null;
-        // определяем индекс
-        int index = hashFunc(key);
-        // нет данных - возвращаем null
-        if (data.get(index) == null) return null;
-        // проверяем ключи всех элементов в bucket
-        LinkedList<Item<K, V>> bucket = data.get(index);
+        V val = null;                                                       // возвращаемое значение (если будет найдено)
+        int index = hashFunc(key);                                          // определяем индекс
+        if (data.get(index) == null) return null;                           // нет данных - возвращаем null
+        LinkedList<Item<K, V>> bucket = data.get(index);                    // проверяем ключи всех элементов в bucket
         for (Item<K, V> item : bucket) {
-            if (item.getKey().equals(key)) { // ключ найден
-                val = item.getValue(); // сохраняем значение
-                bucket.remove(item); // удаляем элемент из списка
-                data.set(index, bucket.size() != 0 ? bucket : null); // возвращаем измененный список или null в массив
+            if (item.getKey().equals(key)) {                                // ключ найден
+                val = item.getValue();                                      // сохраняем значение
+                bucket.remove(item);                                        // удаляем элемент из списка
+                data.set(index, bucket.size() != 0 ? bucket : null);        // возвращаем измененный список или null в массив
             }
         }
-        size--; //уменьшаем размер
-        return val; // возвращаем удаленное значение
+        size--;                                                             //уменьшаем размер
+        return val;                                                         // возвращаем удаленное значение
     }
 
     @Override
